676d7c9495fd003390b7730ed6790f907cf729c9
aj_shang <1037390087@qq.com>
2022-08-27 11:36:20 +0800

    比较含退格的字符串，用到charat()以及双指针法倒着来

--- src/Arrays/two_removeElement/Array02_3.java
+++ src/Arrays/two_removeElement/Array02_3.java
@@ -12,24 +12,67 @@ package Arrays.two_removeElement;
 public class Array02_3 {
     public static boolean backspaceCompare(String s, String t) {
 
-        int p1 = 0;
-        int p2 = 0;
-        char[] chars = s.toCharArray();
-        char[] chart = t.toCharArray();
+        int skips = 0;
+        int skipt = 0;
+        int i = s.length() - 1;
+        int j = t.length() - 1;
 
-        for ( ; p1 < chars.length ; p1++) {
+        //1.大循环比较是否相同
+        while (i >= 0 || j >= 0) {
 
-            if (('#' == chars[p1]) && (0 == p1)) { //当第一个元素为“#”时，数组整体向前移动一位
-                chars[p2] = chars[++p1];
+            //2.内部循环判断字符串s
+            while(i >= 0) {
+                if (s.charAt(i) == '#') { //2.1 当字母为‘#’时，=> i-- , skip++,统计#的个数，之后前移做准备
+                    i--;
+                    skips++;
+                } else if (skips > 0) {//2.2 根据统计#的个数，继续前移，实现#的退格功能
+                    i--;
+                    skips--;
+                } else {    //2.2 当字母不为‘#’时 ，退出循环等待大循环比较
+                    break;
+                }
+
+            }
+
+            //3.内部循环判断字符串t
+            while(j >= 0) {
+                if (t.charAt(j) == '#') { //3.1 当字母为‘#’时，=> i-- , skip++,统计#的个数，之后前移做准备
+                    j--;
+                    skipt++;
+                } else  if (skipt > 0){//3.2 根据统计#的个数，继续前移，实现#的退格功能
+                    j--;
+                    skipt--;
+                } else {     //3.2 当字母不为‘#’时 ，退出循环等待大循环比较
+                    break;
+                }
+            }
+
+            //4.比较两个i，t指向的位置，字母是否相同
+            //4.1 相同就i，t同时往前移一位
+            if (i >= 0 && j >= 0) {
+                if (s.charAt(i) == t.charAt(j)) {
+                    i--;
+                    j--;
+                } else {
+                    //4.2 不同退出循环，返回false
+                    return false;
+                }
+            } else if (i >= 0 || j >= 0) {
+                return false;
             }
         }
 
+        return true;
     }
+
     public static void main(String[] args) {
 
-        String s = "ab#c";
-        String t = "ad#c";
+        //String s = "ab#cd";
+        // String t = "ad#c";
 
-        backspaceCompare(s,t);
+        String s = "bbbextm";
+        String t = "bbb#extm";
+
+        System.out.println(backspaceCompare(s,t));
     }
 }
