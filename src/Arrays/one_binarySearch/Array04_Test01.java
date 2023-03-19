8bf366040047f26adb5f3ab560f89638d94ace5b
PCLenovo-aj_shang <1037390087@qq.com>
2023-03-19 17:10:36 +0800

    练习数组04题，考点双指针法

--- src/Arrays/one_binarySearch/Array04_Test01.java
+++ src/Arrays/one_binarySearch/Array04_Test01.java
@@ -1,8 +1,9 @@
 package Arrays.one_binarySearch;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-/*27. 移除元素
+/*27. 移除元素(双指针法)
 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
 
 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
@@ -18,34 +19,29 @@ import java.util.Scanner;
 链接：https://leetcode.cn/problems/remove-element
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
-public class Array04 {
+public class Array04_Test01 {
 
-    public  static long  mySqrt(int x) {
-
-        long temp = x;
-        long left = temp;
-
-        while(left * left > x) { //先找到距离算术平方根最近的值，比平方根小
-            temp = temp / 2;
-            left = temp;
-            //System.out.println(left);
+    public static int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = 0;
+        while (right < nums.length) {
+            if (val != nums[right]) {
+                nums[left] = nums[right];
+                left++;
+            }
+            right++;
         }
-
-        while (left * left <= x){   //每次一点点递增；算出的left比平方根大一点
-            left += 1;
-        }
-
-        int l = (int)left;
-        return (l - 1);
+        return left;
     }
 
     public static void main(String[] args) {
 
-        System.out.println("请输入x值");
+        //int[] nums = new int[]{0,1,2,2,3,0,4,2};
+        int[] nums = new int[]{3,2,2,3};
+        System.out.println(Arrays.toString(nums));
+        System.out.println(("val = "));
         Scanner sc = new Scanner(System.in);
-        int key = sc.nextInt();
-
-        System.out.println(mySqrt(key));
-
+        int val = sc.nextInt();
+        System.out.println(removeElement(nums, val));
     }
 }
